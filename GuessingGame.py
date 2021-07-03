47b0f2d4f82a2b80fbca995f8b064aa23976c4a1
Muhammad Danial Siddiqui <46169845+BIGG000@users.noreply.github.com>
2021-07-04 01:03:15 +0500

    update

---
+++ GuessingGame.py
@@ -0,0 +1,11 @@
+import random
+
+top_number = input("Enter the number: ")
+
+if top_number.isdigit():
+    top_number = int(top_number)
+
+    if top_number <= 0:
+        print("Enter a large number then 0 next time.")
+else:
+    print("Enter the number next time")
