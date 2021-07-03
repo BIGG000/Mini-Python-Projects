776882315f31271b04db515c3b1e3e9be2f0cdb9
Muhammad Danial Siddiqui <46169845+BIGG000@users.noreply.github.com>
2021-07-04 01:25:38 +0500

    update

--- GuessingGame.py
+++ GuessingGame.py
@@ -8,10 +8,22 @@ if top_number.isdigit():
     if top_number <= 0:
         print("Enter a large number then 0 next time.")
         quit()
-
+        
 else:
     print("Enter the number next time")
     quit()
 
 random_number = random.randint(0, top_number)
-print("Your Random Number is :",random_number)
+guesses = 0
+
+while True:
+    guesses += 1
+    user_guess = input(int("Enter a Number: "))
+    if user_guess == random_number:
+        print("You got it correct")
+        break
+    elif user_guess > random_number:
+        print("You are high on guess number")
+    else:
+        print("You are low on guess number")
+print("You got the answer in ",guesses,"guesses")
